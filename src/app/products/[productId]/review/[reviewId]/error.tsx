1511e07e28c5bdc10e1fbc3719161721914daa1d
imgeekabhi <imgeekabhi@gmail.com>
2024-03-19 13:04:26 +0530

    feat:error handling

---
+++ src/app/products/[productId]/review/[reviewId]/error.tsx
@@ -0,0 +1,8 @@
+"use client";
+import React from "react";
+
+const ErrorBoundries = () => {
+  return <div>Error in review id</div>;
+};
+
+export default ErrorBoundries;
--- src/app/products/[productId]/review/[reviewId]/page.tsx
+++ src/app/products/[productId]/review/[reviewId]/page.tsx
@@ -1,11 +1,27 @@
-import React from 'react'
+import { count } from "console";
+import React from "react";
+import { notFound } from "next/navigation";
+const getRandomInt = (count: number) => {
+  return Math.floor(Math.random() * count);
+};
 
-const ProductReview = ({params}:{
-    params:{productId:number,reviewId:number}
+const ProductReview = ({
+  params,
+}: {
+  params: { productId: number; reviewId: number };
 }) => {
+  const random = getRandomInt(2);
+  if (random === 1) {
+    throw new Error(`Error Loading review`);
+  }
+  if (params.reviewId > 1000) {
+    notFound();
+  }
   return (
-    <div>ProductId {params.productId} ProductReview {params.reviewId}</div>
-  )
-}
+    <div>
+      ProductId {params.productId} ProductReview {params.reviewId}
+    </div>
+  );
+};
 
-export default ProductReview
+export default ProductReview;
