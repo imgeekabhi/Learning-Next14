9fecc91e797bd485d48de4b0d892cfbe5b4f6267
imgeekabhi <imgeekabhi@gmail.com>
2024-03-22 15:11:20 +0530

    feat:Intercepting route on one level up.

---
+++ src/app/f1/f4/(..)f3/page.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const InterceptingF3Page = () => {
+  return <div>Intercepting F3 Page</div>;
+};
+
+export default InterceptingF3Page;
---
+++ src/app/f1/f4/page.tsx
@@ -0,0 +1,13 @@
+import Link from "next/link";
+import React from "react";
+
+const F4Page = () => {
+  return (
+    <div>
+      <div>F4 Page</div>
+      <Link href={`/f1/f3`}>F3</Link>
+    </div>
+  );
+};
+
+export default F4Page;
