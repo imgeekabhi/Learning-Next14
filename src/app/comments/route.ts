615b0fee9ab1207c7674dd73ba80ac840233a29f
abhishek_simform <abhishek.sharma@simformsolutions.com>
2024-03-28 17:18:52 +0530

    feat:query params and redirects route

--- src/app/comments/route.ts
+++ src/app/comments/route.ts
@@ -1,6 +1,13 @@
+import { NextRequest } from "next/server";
 import { comments } from "./data";
-export async function GET() {
-  return Response.json(comments);
+export async function GET(request: NextRequest) {
+  console.log("request", request);
+  const searchParams = request.nextUrl.searchParams;
+  const query = searchParams.get("query");
+  const filteredComments = query
+    ? comments.filter((comment) => comment.text.includes(query))
+    : comments;
+  return Response.json(filteredComments);
 }
 export async function POST(request: Request) {
   const comment = await request.json();
