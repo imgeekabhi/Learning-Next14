3f84fc213fae2ae94e166ca8dc6def7d1f74dbc9
abhishek_simform <abhishek.sharma@simformsolutions.com>
2024-04-16 13:19:14 +0530

    feat:client and server components

---
+++ src/app/dashboard/page.tsx
@@ -0,0 +1,10 @@
+//example of client component
+"use client";
+import React, { useState } from "react";
+
+const DashboardPage = () => {
+  const [name, setName] = useState("");
+  return <div>DashboardPage</div>;
+};
+
+export default DashboardPage;
